4f438cdf5c1c0b6c738f090f69b250ae83fddd08
ElinaBoman <>
2023-10-16 19:36:49 +0000

    Corrected bug that stoped hide-class to stopworking. Status: Fixed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,76 +12,80 @@
     const computerImage = document.getElementById("computer-img");
     const homeUrl = 'https://8000-elinaboman-rps2-2udw505m6g.us2.codeanyapp.com/';
     const homeButton = document.getElementById("home-button");
+    
+  // Add hide function to section.
+    usernameSection.classList.remove("hide");
+    gameRulesSection.classList.add("hide");
+    gameAreaSection.classList.add("hide");
    
 
-   // EventListeners
-
-    // Homebutton
-    homeButton.addEventListener("click", function () {
-     window.location.href = homeUrl;
-    }); 
-    loginForm.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    (usernameInput == "")
-        usernameSection.classList.add("hide");
-        gameRulesSection.classList.remove("hide");
+    document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
+        //const userChoice = button.id;
+        //const computerChoice = computerChoice();
+        //const result = checkWinner(userChoice, computerChoice);
+        //displayResult(userChoice, computerChoice, result);
+     });
     });
 
-    // Add hide function to section.
-        usernameSection.classList.remove("hide");
-        gameRulesSection.classList.add("hide");
-        gameAreaSection.classList.add("hide");
-    
-     // Listens for form, performs no action.
-    
+    loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    usernameSection.classList.add("hide");
+    gameRulesSection.classList.remove("hide");
+    });
 
 // This section will remove the rulearea-section and then show gamearea-section.
-gameRulesSection.addEventListener("click", function (event) {
+    gameRulesSection.addEventListener("click", function (event) {
     event.preventDefault();
     gameRulesSection.classList.add("hide");
     gameAreaSection.classList.remove("hide");
-});
-//This is the array of choices.
-const options = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"];
-
-    document.querySelectorAll("button").forEach((button) => {
-        button.addEventListener("click", () => {
-        const userChoice = button.id;
-        const computerChoice = computerChoose();
-        const result = checkWinner(userChoice, computerChoice);
-        displayResult(userChoice, computerChoice, result);
-
-        });
-
     });
+    // Homebutton
+    homeButton.addEventListener("click", function () {
+     window.location.href = homeUrl;
+    });  
+
+
+
+
+      
+
+
+    
+    
     /* This function is for the computer to make a choice.
      From a random index a hole number is going to be choosen. 
      Then it's going to be multiplied with 5 scince there are 5 different options.*/
+   
 
-    function computerChoose() {
-        const randomIndex = Math.floor(Math.random() * 5);
-        return options[randomIndex];
-    }
-    
-
-     //To change images when clicked. Not working.
-    for (let button of buttons) {
-        button.addEventListener("click", function (){
-        let playerChoice = this.getAttribute ("data-choice");
-        playGame(playerChoice)
-    });
-    }
-
-    function playGame (playerChoice) {
-    playerImage.src= `assets/images/${choices[playerImage]}.png`;
-    playerImage.alt = choices [playerImage];
-    }
     /* This function is to check who the winner is.
      If user choice is rock and the computer choice is scissors or lizard, 
      there is going to bo be a messege: You win!. 
      If the computer choice isn't either of theese 2, there is a else message: You lose!.*/
-    function checkWinner(userChoice, computerChoice) {
+     const options = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"];
+    document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
+        const userChoice = button.id;
+
+        const computerChoice = computerChoose();
+
+        const result = determineWinner(userChoice, computerChoice);
+
+        displayResult(userChoice, computerChoice, result);
+
+    });
+
+    });
+    function computerChoose() {
+         const randomIndex = Math.floor(Math.random() * 5);
+        return options[randomIndex];
+}
+    
+
+
+
+    
+    function determineWinner(userChoice, computerChoice) {
         let isCorrect = (
             (userChoice === "ROCK" && (computerChoice === "SCISSORS" || computerChoice === "LIZARD")) ||
             (userChoice === "PAPER" && (computerChoice === "ROCK" || computerChoice === "SPOCK")) ||
