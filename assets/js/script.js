900b294c2a9455be9a005b7b860fe0b7f89f1b9a
ElinaBoman <>
2023-10-17 14:37:56 +0000

    Fixed error with if statement, got help from Craig Hudson in slackchannel.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,137 @@
-
-     // Constant variables
-    const form = document.getElementById("login-form");
-    const usernameSection = document.getElementById("enter-username-section");
-    const username = document.getElementsByClassName("user-name");
-    const usernameSubmitBtn = document.getElementsByClassName("submit-username-btn");
-    const gameRulesSection = document.getElementById("game-rules-section");
-    const gameAreaSection = document.getElementById("game-area");
-    const playerButton = document.getElementById("playerButton");
-    const startGame = document.getElementsByClassName("game-rules-btn");
-    const playerImage = document.getElementById("player-img");
-    const computerImage = document.getElementById("computer-img");
-    const homeUrl = 'https://8000-elinaboman-rps2-2udw505m6g.us2.codeanyapp.com/';
-    const homeButton = document.getElementById("home-button");
-    const resetScore = document.getElementById("reset-score")
-    const errorElement = document.getElementById("error")
-    //Add hide function to section.
-    usernameSection.classList.remove("hide");
-    gameRulesSection.classList.add("hide");
-    gameAreaSection.classList.add("hide");
-    
-    
-
-    /*FÖR ATT GÖRA HIDE FUNKTIONEN ENKLARE.
-    function showPanel (panel) {
-        panel.classList.remove("show");
-    }
-    function hidePanel (panel) {
-        panel.classList.remove("hide")
-    }
-    */
-    /*
-    showPanel(usernameSection);
-    hidePanel(gameRulesSection);
-    hidePanel(gameAreaSection);*/
-
-    /*document.querySelectorAll("button").forEach((button) => {
-    //button.addEventListener("click", () => {
-        //const userChoice = button.id;
-        //const computerChoice = computerChoice();
-        //const result = checkWinner(userChoice, computerChoice);
-        //displayResult(userChoice, computerChoice, result);
-    // });
-    });*/
-
-    /*loginForm.addEventListener("submit", function (event) {
-        event.preventDefault();
+// Constant variables
+const form = document.getElementById("login-form");
+const usernameSection = document.getElementById("enter-username-section");
+// const username = document.getElementsByClassName("user-name");
+const username = document.getElementById("username-input");
+const usernameSubmitBtn = document.getElementsByClassName("submit-username-btn");
+const gameRulesSection = document.getElementById("game-rules-section");
+const gameAreaSection = document.getElementById("game-area");
+const playerButton = document.getElementById("playerButton");
+const startGame = document.getElementsByClassName("game-rules-btn");
+const playerImage = document.getElementById("player-img");
+const computerImage = document.getElementById("computer-img");
+const homeUrl = 'https://8000-elinaboman-rps2-2udw505m6g.us2.codeanyapp.com/';
+const homeButton = document.getElementById("home-button");
+const resetScore = document.getElementById("reset-score");
+const errorElement = document.getElementById("error");
+//Add hide function to section.
+usernameSection.classList.remove("hide");
+gameRulesSection.classList.add("hide");
+gameAreaSection.classList.add("hide");
+/*FÖR ATT GÖRA HIDE FUNKTIONEN ENKLARE.
+function showPanel (panel) {
+    panel.classList.remove("show");
+}
+function hidePanel (panel) {
+    panel.classList.remove("hide")
+}
+*/
+/*
+showPanel(usernameSection);
+hidePanel(gameRulesSection);
+hidePanel(gameAreaSection);*/
+/*document.querySelectorAll("button").forEach((button) => {
+//button.addEventListener("click", () => {
+    //const userChoice = button.id;
+    //const computerChoice = computerChoice();
+    //const result = checkWinner(userChoice, computerChoice);
+    //displayResult(userChoice, computerChoice, result);
+// });
+});*/
+/*loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    usernameSection.classList.add("hide");
+    gameRulesSection.classList.remove("hide");
+});*/
+// Homebutton
+homeButton.addEventListener("click", function () {
+    window.location.href = homeUrl;
+});
+// This section will remove the rulearea-section and then show gamearea-section.
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let messages = [];
+    const enteredUsername = username.value.trim();
+    if (enteredUsername === "") {
+        messages.push("Please enter a username");
         usernameSection.classList.add("hide");
         gameRulesSection.classList.remove("hide");
-    });*/
-    // Homebutton
-    homeButton.addEventListener("click", function () {
-        window.location.href = homeUrl;
-    });  
-// This section will remove the rulearea-section and then show gamearea-section.
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let messages = []
-        if (username.value === ""){
-            messages.push("Please select username")
-        }
-        username = usernameInput.value.trim();
-        if (username === "") {
-            usernameInput.innerText= `Please enter name`;
-            return;
-        } else {
-            gameRulesSection.classList.remove("hide");
-            loginForm.classList.add("hide");
-        }
-    });
-
-
-    /* This function is to check who the winner is.
-     If user choice is rock and the computer choice is scissors or lizard, 
-     there is going to bo be a messege: You win!. 
-     If the computer choice isn't either of theese 2, there is a else message: You lose!.*/
-    const options = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"];
-    document.querySelectorAll("button").forEach((button) => {
-        button.addEventListener("click", () => {
+    } else {
+        gameRulesSection.classList.remove("hide");
+        usernameSection.classList.add("hide");
+    }
+});
+gameRulesSection.addEventListener("click", function (event) {
+    event.preventDefault();
+    gameRulesSection.classList.add("hide");
+    gameAreaSection.classList.remove("hide");
+});
+/* username = usernameInput.value.trim();
+ if (username === "") {
+     usernameInput.innerText= `Please enter name`;
+     return;
+ } else {
+     gameRulesSection.classList.remove("hide");
+     loginForm.classList.add("hide");
+ }
+});*/
+/* This function is to check who the winner is.
+ If user choice is rock and the computer choice is scissors or lizard,
+ there is going to bo be a messege: You win!.
+ If the computer choice isn't either of theese 2, there is a else message: You lose!.*/
+const options = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"];
+document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
         const userChoice = button.id;
         const computerChoice = computerChoose();
         const result = determineWinner(userChoice, computerChoice);
         //displayResult(userChoice, computerChoice, result);
-
     });
-
-    });
-    /* This function is for the computer to make a choice.
-    From a random index a hole number is going to be choosen. 
-    Then it's going to be multiplied with 5 scince there are 5 different options.*/
-    function computerChoose() {
-        const randomIndex = Math.floor(Math.random() * 5);
-        return options[randomIndex];
+});
+/* This function is for the computer to make a choice.
+From a random index a hole number is going to be choosen.
+Then it's going to be multiplied with 5 scince there are 5 different options.*/
+function computerChoose() {
+    const randomIndex = Math.floor(Math.random() * 5);
+    return options[randomIndex];
 }
-    function determineWinner(userChoice, computerChoice) {
-       
-        let isCorrect = (
-            (userChoice === "ROCK" && (computerChoice === "SCISSORS" || computerChoice === "LIZARD")) ||
-            (userChoice === "PAPER" && (computerChoice === "ROCK" || computerChoice === "SPOCK")) ||
-            (userChoice === "SCISSORS" && (computerChoice === "PAPER" || computerChoice === "LIZARD")) ||
-            (userChoice === "LIZARD" && (computerChoice === "SPOCK" || computerChoice === "PAPER")) ||
-            (userChoice === "SPOCK" && (computerChoice === "SCISSORS" || computerChoice === "ROCK"))
-        )
-        if (userChoice === computerChoice) {
-            return "It's a tie!";
-        }
-       // This adds score to player if isCorrect function is started.
-        else if (isCorrect) {
-            incrementScore(); 
-            return "YOU WIN HOOOOORAY!";
-        } 
-        //This adds score to computer is the earlier statements arn't fired.
-        else if (!(isCorrect || userChoice === computerChoice)){
-            incrementWrongAnswer()    
-            return "You lose!";    
-        }
+function determineWinner(userChoice, computerChoice) {
+    let isCorrect = (
+        (userChoice === "ROCK" && (computerChoice === "SCISSORS" || computerChoice === "LIZARD")) ||
+        (userChoice === "PAPER" && (computerChoice === "ROCK" || computerChoice === "SPOCK")) ||
+        (userChoice === "SCISSORS" && (computerChoice === "PAPER" || computerChoice === "LIZARD")) ||
+        (userChoice === "LIZARD" && (computerChoice === "SPOCK" || computerChoice === "PAPER")) ||
+        (userChoice === "SPOCK" && (computerChoice === "SCISSORS" || computerChoice === "ROCK"))
+    );
+    if (userChoice === computerChoice) {
+        return "It's a tie!";
     }
-    
-    //This shows playsers choice and computer choice.
-    function displayResult(userChoice, computerChoice, result) {
-        const resultElement = document.getElementById("result");
-        resultElement.innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}.<br>${result}`;
+    // This adds score to player if isCorrect function is started.
+    else if (isCorrect) {
+        incrementScore();
+        return "YOU WIN HOOOOORAY!";
     }
-    // Theese are the functions to increment score.
-    function incrementScore() {
-        let playerScore = parseInt(document.getElementById("player-score").innerText);
-        document.getElementById("player-score").innerText = ++playerScore;
+    //This adds score to computer is the earlier statements arn't fired.
+    else if (!(isCorrect || userChoice === computerChoice)) {
+        incrementWrongAnswer();
+        return "You lose!";
     }
-
-    function incrementWrongAnswer(){
-        let computerScore = parseInt(document.getElementById("computer-score").innerText);
-         document.getElementById("computer-score").innerText = ++computerScore;    
-    }
-       
-    
-
-    resetScore.addEventListener("click", () => {
-        document.getElementById("player-score").innerText = "0";
-        document.getElementById("computer-score").innerText = "0";
-    });
-
-
+}
+//This shows playsers choice and computer choice.
+function displayResult(userChoice, computerChoice, result) {
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}.<br>${result}`;
+}
+// Theese are the functions to increment score.
+function incrementScore() {
+    let playerScore = parseInt(document.getElementById("player-score").innerText);
+    document.getElementById("player-score").innerText = ++playerScore;
+}
+function incrementWrongAnswer() {
+    let computerScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++computerScore;
+}
+resetScore.addEventListener("click", () => {
+    document.getElementById("player-score").innerText = "0";
+    document.getElementById("computer-score").innerText = "0";
+});
