5a8a2f3d8475577465a934a9b377963abd1dcfec
ElinaBoman <>
2023-10-20 09:07:29 +0000

    Changed comments in javascript file to be more detaild with better explanations.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,11 @@ gameRulesSection.addEventListener("click", function (event) {
     gameAreaSection.classList.remove("hide");
 });
 
-//This will reset the score when the button: reset score, is clicked.
+/**
+ * This EventListener will reset the score when Reset score-button is clicked.
+ * (player-score) will have the innetext of 0.
+ * (computer-score) will have the innertext 0.
+ */
 resetScore.addEventListener("click", () => {
     document.getElementById("player-score").innerText = "0";
     document.getElementById("computer-score").innerText = "0";
@@ -36,13 +40,22 @@ homeButton.addEventListener("click", function () {
     window.location.href = homeUrl;
 });
 
-//Add hide function to section.
+/**
+ * This block of code will run when the user opens the login form.
+ * It will hide sections of Rules and Game.
+ * The username section will show.
+ */
 usernameSection.classList.remove("hide");
 gameRulesSection.classList.add("hide");
 gameAreaSection.classList.add("hide");
 
-
-// This section will remove the rule-area and then show game-area.
+/**
+ * This code will let the user know that there has to be text in the text-input.
+ * If the textbox is an empty string the innerText will read a message to enter username.
+ * If there is a text input the else code block will run and the hide class will remove from Rule area and be applied to enter
+ * username form. 
+ * p1 is the input text that will be enterd in the game. 
+ */
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     const enterUsername = username.value.trim();
@@ -59,9 +72,14 @@ form.addEventListener("submit", function (event) {
     }
 });
 
-let userTries = [0];
-let maxTries = [11];
-/* Winner check function*/
+//This is to limit number of tries before game ends.
+let userTries = 0;
+let maxTries = 11;
+/**
+ * UserTries has to be less then maxTries. 
+ * If userTries is less then maxTries the if code block will run. 
+ * If UserTries = maxTries the else code block will run and alert the user to check score and reset game.
+ */
 for (button of gameButtons) {
     const userChoice = button.id;
     button.addEventListener("click", function () {
@@ -72,19 +90,25 @@ for (button of gameButtons) {
             displayResult(userChoice, computerChoice, result);
         } else {
             window.alert(`Game over! Check result! If you want to play again, hit the reset button!`)
-            userTries = [0]
-           //if (result = incrementScore [parseInt] > incrementWrongAnswer [parseInt]) {
-               // aler(`you Win!`);
-           //}
+            userTries = 0;
         }
         })};
 
-/*Computer choice function created with ChatGPT */
+/**
+ * Computer choice function created with ChatGPT
+ * This code will make the computer choice random. There are 5 different options to choose from.
+ */
 function computerChoose() {
     const randomIndex = Math.floor(Math.random() * 5);
     return options[randomIndex];
 }
 
+/**
+ * determineWinner compares user and computer choice to determine winner.
+ * @param userChoice refers to the users choice of button in the game.
+ * @param computerChoice this displays the computers choice that comes from the computerChoose function.
+ * @returns will display message in game with result.
+ */
 function determineWinner(userChoice, computerChoice) {
     let isCorrect = (
         (userChoice === "ROCK" && (computerChoice === "SCISSORS" || computerChoice === "LIZARD")) ||
@@ -95,37 +119,43 @@ function determineWinner(userChoice, computerChoice) {
     );
     if (userChoice === computerChoice) {
         return "It's a tie!";
-    }
-    // This adds score to player if isCorrect function is started.
-    else if (isCorrect) {
+    } else if (isCorrect) {
         incrementScore();
         return "YOU WIN HOOOOORAY!";
-    }
-    //This adds score to computer is the earlier statements arn't fired.
-    else if (!(isCorrect || userChoice === computerChoice)) {
+    } else if (!(isCorrect || userChoice === computerChoice)) {
         incrementWrongAnswer();
         return "You lose!";
     }
 }
-//This shows playsers choice and computer choice.
+/**
+ * @param userChoice 
+ * @param computerChoice
+ * @param  result
+ * This will send the result from the game and claim the winner of each round.
+ * @param computerChoiseLowerCase will change the computerChoise tho lower case so the images can be paird with the button.
+ * @param playerImage will set the backround in the user div.
+ * @param computerImage will set the background in the computer div.
+*/
 function displayResult(userChoice, computerChoice, result) {
-    const resultElement = document.getElementById("result");
-
-// This will change the text in the game and show result and what buttons where pressed.
-    resultElement.innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}.<br>${result}`;
-    let playerChoiceLowerCase = userChoice.toLowerCase();
-    let computerChoiceLowerCase = computerChoice.toLowerCase();
-    playerImage.src = `assets/images/${playerChoiceLowerCase}.png`;
-    computerImage.src = `assets/images/${computerChoiceLowerCase}.png`;
+        const resultElement = document.getElementById("result");
+        resultElement.innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}.<br>${result}`;
+        //let playerChoiceLowerCase = userChoice.toLowerCase();
+        let computerChoiceLowerCase = computerChoice.toLowerCase();
+        playerImage.src = `assets/images/${playerChoiceLowerCase}.png`;
+        computerImage.src = `assets/images/${computerChoiceLowerCase}.png`;
 };
 
-// This function increment score.
+/**
+ * This function increment score.
+*/
 function incrementScore() {
     let playerScore = parseInt(document.getElementById("player-score").innerText);
     document.getElementById("player-score").innerText = ++playerScore;
 }
 
-//This function increments computers score, every time the computer wins.
+/**
+ * This function increments computers score, every time the answer is not isCorrect,
+ */
 function incrementWrongAnswer() {
     let computerScore = parseInt(document.getElementById("computer-score").innerText);
     document.getElementById("computer-score").innerText = ++computerScore;
